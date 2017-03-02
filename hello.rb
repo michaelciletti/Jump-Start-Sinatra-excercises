580bd3c5e11e3e037104906e8cd1ed288769b0a8
michaelciletti <michaelciletti@gmail.com>
2017-03-02 11:44:03 -0500

    more fetching data entered in url not working

--- hello.rb
+++ hello.rb
@@ -17,3 +17,9 @@ end
 get '/:one/:two/:three' do
 	"first: #{params[:one]}, second: #{params[:two]}, third: #{params[:three]}"
 end
+
+get '/what/time/is/it/in/ :number/hours' do
+	number = params[:number].to_i
+	time = Time.now + number * 3600
+	"The time in #{number} hours will be #{time.strftime('%T:%M %p')}"
+end
